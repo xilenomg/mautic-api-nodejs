1f9978ac9fc8b6126c4947e59bd213d2996473ec
nachomazzara <imazzara@Ignacios-MacBook-Pro.local>
2016-11-28 20:31:11 -0300

    work on readme

--- src/config/config.js
+++ src/config/config.js
@@ -1,5 +1,6 @@
 module.exports = {
 	MAUTIC_BASE_URL: 'https://mautic.acadevor.com/',
+	MAUTIC_REDIRECT_URI: 'http://localhost:3000/auth/callback',
 	MAUTIC_PUBLIC_KEY: '1_4xs2s4hvzcg8sg4wkk0g0ow0wsgocs0ccgsossg84cw08gso4g',
 	MAUTIC_SECRET_KEY: '3bqbhzbs8vs4o04c84og4ows04wowk8sw40oo0cos8w8s00wo4',
 	ACCESS_TOKEN: 'ODkyZmYwMjhjMWYzOTA1ZmQ4NjEwYmRlNjQzNWIwOTVkNzkwZTkzZmFiMDNkZDdjMmQ4M2RmYzVlYWI1MTRkMQ',
