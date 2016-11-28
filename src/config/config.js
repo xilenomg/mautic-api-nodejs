7e2683cf124f0a039d2511103c08926caef66784
nachomazzara <imazzara@Ignacios-MacBook-Pro.local>
2016-11-28 18:24:45 -0300

    config.js

---
+++ src/config/config.js
@@ -0,0 +1,7 @@
+module.exports = {
+	MAUTIC_BASE_URL: 'https://mautic.acadevor.com/',
+	MAUTIC_PUBLIC_KEY: '1_4xs2s4hvzcg8sg4wkk0g0ow0wsgocs0ccgsossg84cw08gso4g',
+	MAUTIC_SECRET_KEY: '3bqbhzbs8vs4o04c84og4ows04wowk8sw40oo0cos8w8s00wo4',
+	ACCESS_TOKEN: 'ODkyZmYwMjhjMWYzOTA1ZmQ4NjEwYmRlNjQzNWIwOTVkNzkwZTkzZmFiMDNkZDdjMmQ4M2RmYzVlYWI1MTRkMQ',
+	REFRESH_TOKEN: 'M2ZkY2FjYTUzMjM4Yzg5NTAzMWU3ZTYyMzM2ZWYwZmM0N2ZjYTQyYzNhMWUxZTQ4NmUzYzQ5ZTkwZDVhYmExNA',
+};
