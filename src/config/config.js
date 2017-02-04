188da8400c91ece2eba47c1e1d98d2908ea5c527
nachomazzara <ignaciomazzara@hotmail.com>
2017-02-04 18:14:35 -0300

    change to basic auth

--- src/config/config.js
+++ src/config/config.js
@@ -1,8 +1,9 @@
 module.exports = {
 	MAUTIC_BASE_URL: 'https://mautic.acadevor.com/',
 	MAUTIC_REDIRECT_URI: 'http://localhost:3000/auth/callback',
-	MAUTIC_PUBLIC_KEY: '1_4xs2s4hvzcg8sg4wkk0g0ow0wsgocs0ccgsossg84cw08gso4g',
-	MAUTIC_SECRET_KEY: '3bqbhzbs8vs4o04c84og4ows04wowk8sw40oo0cos8w8s00wo4',
+	MAUTIC_PUBLIC_KEY: '2_2rl0621fl9kwo4s4kcowscog8w4gss0cockg8wkgs48k0so0og',
+	MAUTIC_SECRET_KEY: '61np99qz40coc0008kcow0wgw4gssss8g8gkkwcw0okcwck0og',
 	ACCESS_TOKEN: 'ODkyZmYwMjhjMWYzOTA1ZmQ4NjEwYmRlNjQzNWIwOTVkNzkwZTkzZmFiMDNkZDdjMmQ4M2RmYzVlYWI1MTRkMQ',
 	REFRESH_TOKEN: 'M2ZkY2FjYTUzMjM4Yzg5NTAzMWU3ZTYyMzM2ZWYwZmM0N2ZjYTQyYzNhMWUxZTQ4NmUzYzQ5ZTkwZDVhYmExNA',
+	BASIC_AUTH: 'bHVjYXM6Mzk4NXl0NGpzaw==',
 };
